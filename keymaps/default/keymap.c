4d234fda60109234c91f426d52ac75ca2425b578
Edgar Shport <eshport@enlil.com>
2022-10-03 23:28:23 +0300

    Keymap file

---
+++ keymaps/default/keymap.c
@@ -0,0 +1,265 @@
+ /* Copyright 2021 OpenAnnePro community
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
+#include QMK_KEYBOARD_H
+
+enum anne_pro_layers {
+    BASE,
+    FN1,
+    FN2,
+};
+
+
+// Tap Dances Names
+enum tap_dance_codes {
+  TD_F1,
+  TD_F2,
+  TD_F3,
+  TD_F4,
+};
+
+// clang-format off
+// Key symbols are based on QMK. Use them to remap your keyboard
+/*
+* Layer BASE
+* ,-----------------------------------------------------------------------------------------.
+* | esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |    Bksp   |
+* |-----------------------------------------------------------------------------------------+
+* | Tab    |  q  |  w  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |  [  |  ]  |   \    |
+* |-----------------------------------------------------------------------------------------+
+* | Caps    |  a  |  s  |  d  |  f  |  g  |  h  |  j  |  k  |  l  |  ;  |  '  |    Enter    |
+* |-----------------------------------------------------------------------------------------+
+* | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |  .  |  /  |    Shift       |
+* |-----------------------------------------------------------------------------------------+
+* | Ctrl  |  L1   |  Alt  |               space             |  Alt  |  FN1  |  FN2  | Ctrl  |
+* \-----------------------------------------------------------------------------------------/
+* Layer TAP in BASE
+* ,-----------------------------------------------------------------------------------------.
+* |     |     |     |     |     |     |     |     |     |     |     |     |     |           |
+* |-----------------------------------------------------------------------------------------+
+* |        |     |     |     |     |     |     |     |     |     |     |     |     |        |
+* |-----------------------------------------------------------------------------------------+
+* |         |     |     |     |     |     |     |     |     |     |     |     |             |
+* |-----------------------------------------------------------------------------------------+
+* |            |     |     |     |     |     |     |     |     |     |     |       UP       |
+* |-----------------------------------------------------------------------------------------+
+* |       |       |       |                                 |       |  LEFT | DOWN  | RIGHT |
+* \-----------------------------------------------------------------------------------------/
+*/
+ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+ [BASE] = LAYOUT_60_ansi( /* Base */
+// |---------------|---------------|---------------|---------------|---------------|---------------|---------------|---------------|---------------|---------------|---------------|---------------|---------------|---------------|
+    KC_ESC,             TD(TD_F1),       TD(TD_F2),           TD(TD_F3),           TD(TD_F4),           KC_5,           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINS,        KC_EQL,         KC_BSPC,
+    KC_TAB,             KC_Q,       KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_LBRC,        KC_RBRC,        KC_BSLS,
+    LT(FN1, KC_CAPS),   KC_A,       KC_S,           KC_D,           KC_F,           KC_G,           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOT,        KC_ENT,
+    KC_LSFT,            KC_Z,       KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,        RSFT_T(KC_UP),
+    KC_LCTL,            KC_LGUI,    KC_LALT,        KC_SPC,         KC_RALT,        LT(FN1, KC_LEFT),LT(FN2, KC_DOWN), RCTL_T(KC_RGHT)
+),
+  /*
+  * Layer FN1
+  * ,-----------------------------------------------------------------------------------------.
+  * |  `  |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE   |
+  * |-----------------------------------------------------------------------------------------+
+  * | Tab    |  q  | UP  |  e  |  r  |  t  |  y  |  u  |  i  |  o  | PS | HOME | END |   \    |
+  * |-----------------------------------------------------------------------------------------+
+  * | Esc     |LEFT |DOWN |RIGHT|  f  |  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
+  * |-----------------------------------------------------------------------------------------+
+  * | Shift      |V-UP |V-DWN|MUTE |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
+  * |-----------------------------------------------------------------------------------------+
+  * | Ctrl  |  L1   |  Alt  |               space             |  Alt  |  FN1  |  FN2  | Ctrl  |
+  * \-----------------------------------------------------------------------------------------/
+  *
+  */
+ [FN1] = LAYOUT_60_ansi( /* FN1 */
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+    _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_HOME, KC_END,  _______,
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______,
+    _______,          KC_VOLU, KC_VOLD, KC_MUTE, _______, _______, _______, _______, _______, KC_INS,  KC_DEL,  _______,
+    _______, _______, _______,                            _______,                   _______, _______, MO(FN2), _______
+),
+  /*
+  * Layer FN2
+  * ,-----------------------------------------------------------------------------------------.
+  * |  ~  | BT1 | BT2 | BT3 | BT4 |  F5 |  F6 | F7 | F8 | MOD | TOG | BRI- | BRI+ |    Bksp   |
+  * |-----------------------------------------------------------------------------------------+
+  * | Tab    |  q  | UP  |  e  |  r  |  t  |  y  |  u  |  i  |  o  | PS | HOME | END |   \    |
+  * |-----------------------------------------------------------------------------------------+
+  * | Esc     |LEFT |DOWN |RIGHT|  f  |  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
+  * |-----------------------------------------------------------------------------------------+
+  * | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
+  * |-----------------------------------------------------------------------------------------+
+  * | Ctrl  |  L1   |  Alt  |               space             |  Alt  |  FN1  |  FN2  | Ctrl  |
+  * \-----------------------------------------------------------------------------------------/
+  *
+  */
+ [FN2] = LAYOUT_60_ansi( /* FN2 */
+    _______, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, _______, _______, _______, _______, RGB_TOG, RGB_MODE_FORWARD, RGB_HUI, RGB_VAI, _______,
+    MO(FN2), _______,    KC_UP,      _______,    _______,    _______, _______, _______, _______, _______,       KC_PSCR,       KC_HOME,       KC_END,        _______,
+    _______, KC_LEFT,    KC_DOWN,    KC_RGHT,    _______,    _______, _______, _______, _______, _______,       KC_PGUP,       KC_PGDN,       _______,
+    _______,             _______,    _______,    _______,    _______, _______, _______, _______, _______,       KC_INS,        KC_DEL,        _______,
+    _______, _______,    _______,                                     _______,                   _______,       MO(FN1),       MO(FN2),       _______
+ ),
+};
+// clang-format on
+
+//
+//// rgb
+
+//void keyboard_post_init_user(void) {
+//  ap2_led_enable();
+//  ap2_led_set_profile(1);
+//}
+
+
+// Tap Dance Logic Don't Touch That
+typedef struct {
+    bool is_press_action;
+    uint8_t step;
+} tap;
+
+enum {
+    SINGLE_TAP = 1,
+    SINGLE_HOLD,
+    DOUBLE_TAP,
+    DOUBLE_HOLD,
+    DOUBLE_SINGLE_TAP,
+    TRIPLE_TAP,
+    TRIPLE_HOLD,
+    TRIPLE_SINGLE_TAP,
+    MORE_TAPS
+};
+
+static tap dance_state[9];
+uint8_t dance_step(qk_tap_dance_state_t *state);
+uint8_t dance_step(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return SINGLE_TAP;
+        else return SINGLE_HOLD;
+    } else if (state->count == 2) {
+        if (state->pressed) return DOUBLE_HOLD;
+        else return DOUBLE_TAP;
+    } else if (state->count == 3) {
+        if (state->pressed) return TRIPLE_HOLD;
+        else return TRIPLE_TAP;
+    }
+    return MORE_TAPS;
+}
+
+
+
+// Tap Dances Functions
+void on_dance_0(qk_tap_dance_state_t *state, void *user_data);
+void dance_0_finished(qk_tap_dance_state_t *state, void *user_data);
+void dance_0_reset(qk_tap_dance_state_t *state, void *user_data);
+void on_dance_0(qk_tap_dance_state_t *state, void *user_data) {}
+void dance_0_finished(qk_tap_dance_state_t *state, void *user_data) {
+    dance_state[0].step = dance_step(state);
+    switch (dance_state[0].step) {
+        case SINGLE_TAP: register_code16(KC_1); break;
+        case DOUBLE_TAP: register_code16(KC_1); break;
+        case SINGLE_HOLD: register_code16(KC_F1); break;
+        case DOUBLE_HOLD: register_code16(KC_1); break;
+    }
+}
+void dance_0_reset(qk_tap_dance_state_t *state, void *user_data) {
+    wait_ms(10);
+    switch (dance_state[0].step) {
+        case SINGLE_TAP: unregister_code16(KC_1); break;
+        case DOUBLE_TAP: unregister_code16(KC_1); break;
+        case SINGLE_HOLD: unregister_code16(KC_F1); break;
+        case DOUBLE_HOLD: unregister_code16(KC_1); break;
+    }
+    dance_state[0].step = 0;
+}
+
+void on_dance_1(qk_tap_dance_state_t *state, void *user_data);
+void dance_1_finished(qk_tap_dance_state_t *state, void *user_data);
+void dance_1_reset(qk_tap_dance_state_t *state, void *user_data);
+void on_dance_1(qk_tap_dance_state_t *state, void *user_data) {}
+void dance_1_finished(qk_tap_dance_state_t *state, void *user_data) {
+    dance_state[1].step = dance_step(state);
+    switch (dance_state[1].step) {
+        case SINGLE_TAP: register_code16(KC_2); break;
+        case DOUBLE_TAP: register_code16(KC_2); break;
+        case SINGLE_HOLD: register_code16(KC_F2); break;
+        case DOUBLE_HOLD: register_code16(KC_2); break;
+    }
+}
+void dance_1_reset(qk_tap_dance_state_t *state, void *user_data) {
+    wait_ms(10);
+    switch (dance_state[1].step) {
+        case SINGLE_TAP: unregister_code16(KC_2); break;
+        case DOUBLE_TAP: unregister_code16(KC_2); break;
+        case SINGLE_HOLD: unregister_code16(KC_F2); break;
+        case DOUBLE_HOLD: unregister_code16(KC_2); break;
+    }
+    dance_state[1].step = 0;
+}
+
+void on_dance_2(qk_tap_dance_state_t *state, void *user_data);
+void dance_2_finished(qk_tap_dance_state_t *state, void *user_data);
+void dance_2_reset(qk_tap_dance_state_t *state, void *user_data);
+void on_dance_2(qk_tap_dance_state_t *state, void *user_data) {}
+void dance_2_finished(qk_tap_dance_state_t *state, void *user_data) {
+    dance_state[2].step = dance_step(state);
+    switch (dance_state[2].step) {
+        case SINGLE_TAP: register_code16(KC_3); break;
+        case DOUBLE_TAP: register_code16(KC_3); break;
+        case SINGLE_HOLD: register_code16(KC_F3); break;
+        case DOUBLE_HOLD: register_code16(KC_3); break;
+    }
+}
+void dance_2_reset(qk_tap_dance_state_t *state, void *user_data) {
+    wait_ms(10);
+    switch (dance_state[2].step) {
+        case SINGLE_TAP: unregister_code16(KC_3); break;
+        case DOUBLE_TAP: unregister_code16(KC_3); break;
+        case SINGLE_HOLD: unregister_code16(KC_F3); break;
+        case DOUBLE_HOLD: unregister_code16(KC_3); break;
+    }
+    dance_state[2].step = 0;
+}
+
+void on_dance_3(qk_tap_dance_state_t *state, void *user_data);
+void dance_3_finished(qk_tap_dance_state_t *state, void *user_data);
+void dance_3_reset(qk_tap_dance_state_t *state, void *user_data);
+void on_dance_3(qk_tap_dance_state_t *state, void *user_data) {}
+void dance_3_finished(qk_tap_dance_state_t *state, void *user_data) {
+    dance_state[3].step = dance_step(state);
+    switch (dance_state[3].step) {
+        case SINGLE_TAP: register_code16(KC_4); break;
+        case DOUBLE_TAP: register_code16(KC_4); break;
+        case SINGLE_HOLD: register_code16(KC_F4); break;
+        case DOUBLE_HOLD: register_code16(KC_4); break;
+    }
+}
+void dance_3_reset(qk_tap_dance_state_t *state, void *user_data) {
+    wait_ms(10);
+    switch (dance_state[3].step) {
+        case SINGLE_TAP: unregister_code16(KC_4); break;
+        case DOUBLE_TAP: unregister_code16(KC_4); break;
+        case SINGLE_HOLD: unregister_code16(KC_F4); break;
+        case DOUBLE_HOLD: unregister_code16(KC_4); break;
+    }
+    dance_state[3].step = 0;
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+        [TD_F1] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_0, dance_0_finished, dance_0_reset),
+        [TD_F2] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_1, dance_1_finished, dance_1_reset),
+        [TD_F3] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_2, dance_2_finished, dance_2_reset),
+        [TD_F4] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_3, dance_3_finished, dance_3_reset),
+};
